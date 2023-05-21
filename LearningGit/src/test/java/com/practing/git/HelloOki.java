04ab1e36f1256a072d2fca0ff1afb67f1ab30a23
okyan <okyan@192.168.1.23>
2023-05-20 21:41:51 -0400

    Adding addition method& calling method

--- LearningGit/src/test/java/com/practing/git/HelloOki.java
+++ LearningGit/src/test/java/com/practing/git/HelloOki.java
@@ -5,5 +5,15 @@ public class HelloOki {
 	
 	public static void main(String[] args) {
 		System.out.println("Hey, oki");
+	
+	
+		HelloOki myHwlloOki = new HelloOki();
+		myHwlloOki.addition(5, 4);
+	}
+	
+	public void addition(int num1, int num2) {
+		int result = num1 + num2;
+		
+		System.out.println("num1 +num2 =" + result );
 	}
 }
